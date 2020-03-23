a51f249722f1ed6ffb5b4ceaef6868132aeedd3e
Zuray David <intheblackworld@gmail.com>
2020-03-23 22:02:13 +0800

    修改 meta info

--- src/info/index.js
+++ src/info/index.js
@@ -19,10 +19,10 @@ export default {
     ['銷售專線', '(02)2943-0688'],
   ],
 
-  gtmCode: ['NSN7ZP6'], // 可放置多個
-  recaptcha_site_key_v2: '6Lep-78UAAAAAMaZLtddpvpixEb8cqu7v7758gLz',
-  recaptcha_site_key: '6Lck-L8UAAAAABxfvNA1NJuBrdl3iLFc3GkudC8s', // recaptcha v3
-  recaptcha_user_token: '6Lck-L8UAAAAAIcvenwGDl8_Q1tKbrEYsKuriePa',
+  gtmCode: [''], // 可放置多個
+  recaptcha_site_key_v2: '',
+  recaptcha_site_key: '', // recaptcha v3
+  recaptcha_user_token: '',
   order: {
     title: '賞屋好禮月月抽',
     subTitle: '馬上預約'
